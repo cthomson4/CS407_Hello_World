a14cb332f54c14f4d72a982549d52e807bb6ca00
Cole Thomson <cthomson4@wisc.edu>
2020-01-30 16:03:17 -0600

    Commit made from Windows Laptop

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     ImageView imgView; // added this thing
     // Hello World - Lab 1
 
+    // Edits made on Windows Laptop
+    ImageView imgView2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
